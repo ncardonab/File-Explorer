b7f9beeed16fc07cf13fc2ce1ced320325edb25b
ncardonab <ncardonab@unal.edu.co>
2020-02-21 11:17:08 -0500

    Sidebar menu toggle functionality created and their templates

---
+++ public/main.js
@@ -0,0 +1,7 @@
+// hamburguer menu function
+document.querySelector(".hamburguer-menu").addEventListener("click", event => {
+  console.log("works");
+  const sidebar = document.querySelector(".side-bar");
+  document.querySelector(".hamburguer-menu").classList.toggle("change");
+  sidebar.classList.toggle("change");
+});
