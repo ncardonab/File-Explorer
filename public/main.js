537269d53f13cc25adbe8ff8fcc9052aeba8c71f
ncardonab <ncardonab@unal.edu.co>
2020-02-25 20:11:51 -0500

    Created right click (context menu) UI & functions to show and hide the context menu

--- public/main.js
+++ public/main.js
@@ -1,11 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("off");
+
+  let files = document.querySelectorAll(".file");
+  files.forEach(file => file.addEventListener("contextmenu", showMenu));
+
+  let folders = document.querySelectorAll(".folder");
+  folders.forEach(folder => folder.addEventListener("contextmenu", showMenu));
+
+  menu.addEventListener("mouseleave", hideMenu);
+});
+
+window.addEventListener("contextmenu", event => {
+  event.preventDefault();
+});
+
 // hamburguer menu function
 document.querySelector(".hamburguer-menu").addEventListener("click", event => {
-  console.log("works");
   const sidebar = document.querySelector(".side-bar");
   document.querySelector(".hamburguer-menu").classList.toggle("change");
   sidebar.classList.toggle("change");
 });
 
-window.addEventListener("click", event => {
-  console.log(event);
-});
+function showMenu(event) {
+  event.preventDefault();
+
+  console.log(event.clientX, event.clientY);
+  let menu = document.querySelector(".menu");
+  menu.classList.remove("off");
+  menu.style.top = `${event.clientY}px`;
+  menu.style.left = `${event.clientX}px`;
+}
+
+function hideMenu() {
+  const menu = document.querySelector(".menu");
+  menu.classList.add("off");
+}
