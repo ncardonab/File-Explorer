6dcb4c4b4d6be91bfd1fd45e3e4b3d2a62f4e8ae
ncardonab <ncardonab@unal.edu.co>
2020-03-01 08:29:49 -0500

    Encapsulating everything in classes as static methods and refactoring a lot

--- public/main.js
+++ public/main.js
@@ -1,12 +1,119 @@
+class File {
+  constructor(name, extension, type, permissions, owner) {
+    this.name = name;
+    this.extension = extension;
+    this.type = type;
+    this.permissions = permissions;
+    this.owner = owner;
+  }
+}
+
+class Folder {
+  constructor(name, type, permissions, owner) {
+    this.name = name;
+    this.type = type;
+    this.permissions = permissions;
+    this.owner = owner;
+  }
+}
+
+class UI {
+  static showMenu(event) {
+    event.preventDefault();
+    let menu = document.querySelector(".menu");
+    menu.classList.remove("off");
+    menu.style.top = `${event.clientY}px`;
+    menu.style.left = `${event.clientX}px`;
+  }
+
+  static hideMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.add("off");
+  }
+
+  static renderFilesAndFolders(info) {
+    // Creates the card
+    const icon =
+      info.type === "file"
+        ? `<i class="far fa-file-word"></i>
+           <p>${info.name}${info.extension}</p>`
+        : `<i class="fas fa-folder"></i>
+           <p>${info.name}</p>`;
+
+    const card = `
+    <div class="${info.type}">
+      ${icon}
+    </div>`;
+
+    const filesContainer = document.querySelector(".files-container");
+
+    filesContainer.innerHTML += card;
+  }
+}
+
+class API {
+  static getFilesAndFolders(directory) {
+    let filesAndFolders = [];
+    return fetch(`http://localhost:3000/api/files?directory=${directory}`)
+      .then(response => response.json())
+      .then(objects => {
+        objects.map(object => {
+          if (object.filename) {
+            const { filename, extension, type, permissions, owner } = object;
+            filesAndFolders.push(
+              new File(filename, extension, type, permissions, owner)
+            );
+          } else {
+            const { directory_name, type, permissions, owner } = object;
+            filesAndFolders.push(
+              new Folder(directory_name, type, permissions, owner)
+            );
+          }
+        });
+        return filesAndFolders;
+      });
+  }
+
+  static getFileOrFolder(filesOrFolders, event) {
+    // Current class element is the card itself or the childs
+    const elementClass =
+      event.target.className === "file" ||
+      event.target.className === "directory"
+        ? event.target.className
+        : event.target.parentElement.className;
+    const element =
+      event.target.className === "directory" ||
+      event.target.className === "file"
+        ? event.target.children[1]
+        : event.target.parentElement.children[1];
+    let item;
+    if (elementClass === "file") {
+      // Gets the filename without the extension
+      const filename = element.textContent.slice(
+        0,
+        element.textContent.indexOf(".")
+      );
+      // Filter by the element filename
+      item = filesOrFolders.filter(file => file.name === filename);
+    } else if (elementClass === "directory") {
+      // Gets the directory name
+      const directoryName = element.textContent;
+      // Filter by the element directory name
+      item = filesOrFolders.filter(folder => folder.name === directoryName);
+    }
+    return item;
+  }
+}
+
 document.addEventListener("DOMContentLoaded", () => {
   // hiding the context menu
   const menu = document.querySelector(".menu");
   menu.classList.add("off");
 
-  fetchInfo("home/nicolas").then(filesAndFolders => {
+  API.getFilesAndFolders("home/nicolas").then(filesAndFolders => {
     filesAndFolders.map(info => {
       // Rendering the info in the UI
-      renderFilesAndFolders(info);
+      UI.renderFilesAndFolders(info);
     });
 
     // Listener for file type
@@ -14,8 +121,8 @@ document.addEventListener("DOMContentLoaded", () => {
     files.forEach(file =>
       file.addEventListener("contextmenu", event => {
         // showing the context menu
-        showMenu(event);
-        console.log(filterFilesOrFolders(filesAndFolders, event)[0].filename);
+        UI.showMenu(event);
+        console.log(API.getFileOrFolder(filesAndFolders, event)[0].name);
       })
     );
 
@@ -24,15 +131,13 @@ document.addEventListener("DOMContentLoaded", () => {
     folders.forEach(folder =>
       folder.addEventListener("contextmenu", event => {
         // showing the context menu
-        showMenu(event);
-        console.log(
-          filterFilesOrFolders(filesAndFolders, event)[0].directory_name
-        );
+        UI.showMenu(event);
+        console.log(API.getFileOrFolder(filesAndFolders, event)[0].name);
       })
     );
 
     // hide the menu when the mouse leave the menu
-    menu.addEventListener("mouseleave", hideMenu);
+    menu.addEventListener("mouseleave", UI.hideMenu);
   });
 });
 
@@ -46,71 +151,3 @@ document.querySelector(".hamburguer-menu").addEventListener("click", event => {
   document.querySelector(".hamburguer-menu").classList.toggle("change");
   sidebar.classList.toggle("change");
 });
-
-function filterFilesOrFolders(filesOrFolders, event) {
-  // Current class element is the card itself or the childs
-  const elementClass =
-    event.target.className === "file" || event.target.className === "directory"
-      ? event.target.className
-      : event.target.parentElement.className;
-  const element =
-    event.target.className === "directory" || event.target.className === "file"
-      ? event.target.children[1]
-      : event.target.parentElement.children[1];
-  let item;
-  if (elementClass === "file") {
-    // Gets the filename without the extension
-    const filename = element.textContent.slice(
-      0,
-      element.textContent.indexOf(".")
-    );
-    // Filter by the element filename
-    item = filesOrFolders.filter(object => object.filename === filename);
-  } else if (elementClass === "directory") {
-    // Gets the directory name
-    const directoryName = element.textContent;
-    // Filter by the element directory name
-    item = filesOrFolders.filter(
-      object => object.directory_name === directoryName
-    );
-  }
-  return item;
-}
-
-function showMenu(event) {
-  event.preventDefault();
-  let menu = document.querySelector(".menu");
-  menu.classList.remove("off");
-  menu.style.top = `${event.clientY}px`;
-  menu.style.left = `${event.clientX}px`;
-}
-
-function hideMenu() {
-  const menu = document.querySelector(".menu");
-  menu.classList.add("off");
-}
-
-function renderFilesAndFolders(info) {
-  // Creates the card
-  const icon =
-    info.type === "file"
-      ? `<i class="far fa-file-word"></i>
-         <p>${info.filename}${info.extension}</p>`
-      : `<i class="fas fa-folder"></i>
-         <p>${info.directory_name}</p>`;
-
-  const card = `
-  <div class="${info.type}">
-    ${icon}
-  </div>`;
-
-  const filesContainer = document.querySelector(".files-container");
-
-  filesContainer.innerHTML += card;
-}
-
-function fetchInfo(directory) {
-  return fetch(`http://localhost:3000/api/files?directory=${directory}`)
-    .then(response => response.json())
-    .then(data => data);
-}
